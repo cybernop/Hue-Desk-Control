aafc93de52355e364a789562e5081e40f4a9fff3
Alexander Schulze <cibernop@gmail.com>
2021-11-08 20:30:48 +0100

    init

---
+++ hue_desk_control.py
@@ -0,0 +1,37 @@
+import json
+from pathlib import Path
+
+from phue import Bridge
+
+
+class Hue_Control:
+    bridge: Bridge = None
+
+    def __init__(self, ip: str):
+        self.bridge = Bridge(ip)
+        self.bridge.connect()
+
+    def turn_on(self, group: str):
+        self.__switch(group, True)
+
+    def turn_off(self, group: str):
+        self.__switch(group, False)
+
+    def __switch(self, group_name: str, on: bool):
+        group = self.bridge.get_group(group_name)
+        lights = self.bridge.get_light_objects('id')
+        for light_id in group['lights']:
+            lights[int(light_id)].on = on
+
+
+if __name__ == "__main__":
+    import time
+
+    config = json.loads(Path('config.json').read_text())
+    ip = config['ip']
+    group = config['group']
+
+    control = Hue_Control(ip)
+    control.turn_on(group)
+    time.sleep(1)
+    control.turn_off(group)
